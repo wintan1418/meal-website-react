b5ab7ec14ee9b6ec8a6e11746b72f0f4a025b028
wintan1418 <wintan1418@gmail.com>
2024-07-12 16:53:52 -0500

    Update

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../styles/Footer.css'
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='socialMedia'>
+            <InstagramIcon/>
+            <FacebookIcon/>
+            <TwitterIcon/>
+            <LinkedInIcon/>
+          <p> &copy; 2024 wintech.com </p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
